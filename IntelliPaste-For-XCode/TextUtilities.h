ac60ee26dfc3d7395a628528f5d46a92b34b0566
Robert Gummesson <robert@canemedia.co.uk>
2014-05-24 10:11:59 +0100

    Added support for RGB colors

---
+++ IntelliPaste-For-XCode/TextUtilities.h
@@ -0,0 +1,15 @@
+//
+//  TextUtilities.h
+//  IntelliPaste
+//
+//  Created by Robert Gummesson on 24/05/2014.
+//  Copyright (c) 2014 Cane Media Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TextUtilities : NSObject
+
++ (NSString *)colorsFromText:(NSString *)text;
+
+@end
---
+++ IntelliPaste-For-XCode/TextUtilities.m
@@ -0,0 +1,34 @@
+//
+//  TextUtilities.m
+//  IntelliPaste
+//
+//  Created by Robert Gummesson on 24/05/2014.
+//
+
+#import "TextUtilities.h"
+
+@implementation TextUtilities
+
++ (NSString *)colorsFromText:(NSString *)text
+{
+    if (text.length > 15) {
+        return nil;
+    }
+    
+    NSRegularExpression *const regex = [NSRegularExpression regularExpressionWithPattern:@"(\\d{1,3}),+\\s*(\\d{1,3}),+\\s*(\\d{1,3})" options:0 error:nil];
+    
+    __block NSArray *rgb = nil;
+    [regex enumerateMatchesInString:text options:0 range:NSMakeRange(0, text.length) usingBlock:^(NSTextCheckingResult *result, NSMatchingFlags flags, BOOL *stop) {
+        
+        rgb = @[ [text substringWithRange:[result rangeAtIndex:1]],
+                 [text substringWithRange:[result rangeAtIndex:2]],
+                 [text substringWithRange:[result rangeAtIndex:3]] ];
+    }];
+    
+    if (!rgb) {
+        return nil;
+    }
+    return [NSString stringWithFormat:@"[UIColor colorWithRed:%@./255. green:%@./255. blue:%@/255.]", rgb[0], rgb[1], rgb[2]];
+}
+
+@end
